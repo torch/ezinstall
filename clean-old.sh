635ecdd6bf06efe560d9c01d34f27364e47d147d
Alfredo Canziani <alfredo.canziani@gmail.com>
2014-03-22 21:37:50 -0400

    Improved script std output

--- clean-old.sh
+++ clean-old.sh
@@ -3,9 +3,13 @@
 # Removes all the files Torch related coming from a prev. install
 ######################################################################
 
+# Some space
+echo
+
 # Check if we are on Mac (OS X)
 if [[ `uname` == 'Darwin' ]]; then
-    echo 'Removing files from your Mac...'
+    echo 'Removing old Torch files from your Mac...'
+    echo
     rm -rf ~/.luarocks
     rm -rf /usr/local/lib/luarocks/
     rm -rf /usr/local/lib/lua/
@@ -19,7 +23,8 @@ fi
 
 # Check if we are on Linux (Ubuntu)
 if [[ `uname` == 'Linux' ]]; then
-    echo 'Removing files from your Linux...'
+    echo 'Removing old Torch files from your Linux...'
+    echo
     sudo rm -rf ~/.luarocks
     sudo rm -rf /usr/local/lib/luarocks/
     sudo rm -rf /usr/local/lib/lua/
@@ -31,5 +36,7 @@ if [[ `uname` == 'Linux' ]]; then
     sudo rm     /usr/local/lib/{*lua*,*TH*}
 fi
 
+echo
 echo 'All files from previous installation/s have been removed from your system.'
 echo 'You can now install the new Torch7! :)'
+echo
